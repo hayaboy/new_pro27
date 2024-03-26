a89131d8c4cb81fea227891bc3a01bdddf10968e
hayaboy <hayaboy@korea.ac.kr>
2024-03-26 17:48:26 +0900

    3_26_4

--- src/main/java/com/myspring/common/FileDownloadController.java
+++ src/main/java/com/myspring/common/FileDownloadController.java
@@ -3,6 +3,7 @@ package com.myspring.common;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
+import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,6 +27,7 @@ public class FileDownloadController {
 		logger.info("imageFileName : " + imageFileName);
 
 		String downFile = CURR_IMAGE_REPO_PATH + "\\" + imageFileName;
+		String encodedFileName = URLEncoder.encode(downFile, "UTF-8");
 		// HTTP 응답 헤더에 "Cache-Control" 필드를 설정하여 캐시 제어를 지정하는 것을 의미합니다.
 		// "no-cache" 값은 캐시된 응답을 사용하지 않고 항상 서버로부터 새로운 응답을 요청해야 함을 나타냅니다.
 		// 이렇게 설정하면 브라우저는 캐시된 버전을 사용하지 않고 항상 서버로부터 새로운 데이터를 가져오게 됩니다.
@@ -37,7 +39,7 @@ public class FileDownloadController {
 		// 여기서는 변수 fileName에 저장된 값을 사용하여 다운로드되는 파일의 이름을 동적으로 설정하고 있습니다.
 		// 이렇게 설정된 헤더를 받은 브라우저는 응답을 파일로 다운로드하게 됩니다.
 
-		response.addHeader("Content-disposition", "attachment; fileName=" + imageFileName);
+		response.addHeader("Content-disposition", "attachment; fileName=" + encodedFileName);
 
 		System.out.println("서버에 있는 다운 파일" + downFile);
 
@@ -50,6 +52,13 @@ public class FileDownloadController {
 // 파일에 들어갈 파일 인풋스트림객체 생성
 		FileInputStream in = new FileInputStream(f);
 
+		try {
+			
+		}catch(Exception e) {
+			logger.info("다운 관련 예외이나 크게 상관 없음");
+		}
+		
+		
 		byte[] buffer = new byte[1024 * 8];
 
 		while (true) {
